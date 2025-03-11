a05edd2e424932de0d191905d6827e43b8d4e498
adqm0001 <adam.benmoussa04@gmail.com>
2025-03-10 23:14:43 -0400

    Sec

--- main.py
+++ main.py
@@ -122,11 +122,10 @@ def delete_user_data(user_id):
 # Configure headless Chrome options
 def get_chrome_options():
     chrome_options = Options()
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=1920,1080")
     return chrome_options
 
 
